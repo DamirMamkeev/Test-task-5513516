cda14a8a8064b84d4a5014f54a5cba2ddc089ce6
Damir Mamkeev <mamkeevdamir@gmail.com>
2022-06-05 23:32:07 +0300

    initial commit

---
+++ src/test/java/GoogleCalk/SearchCalc.java
@@ -0,0 +1,35 @@
+package GoogleCalk;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SearchCalc extends TestBase {
+
+
+    @Test
+    public void searchCalculator() {
+        driver.get(SITE_URL);
+
+        driver.findElement(By.xpath("//input[@class = 'gLFyf gsfi']")).sendKeys("Калькулятор");
+        driver.findElement(By.className("gNO89b")).click();
+
+        driver.findElement(By.xpath("//div[text() = '1']")).click();
+        driver.findElement(By.xpath("//div[text() = '×']")).click();
+        driver.findElement(By.xpath("//div[text() = '2']")).click();
+        driver.findElement(By.xpath("//div[text() = '−']")).click();
+        driver.findElement(By.xpath("//div[text() = '3']")).click();
+        driver.findElement(By.xpath("//div[text() = '+']")).click();
+        driver.findElement(By.xpath("//div[text() = '1']")).click();
+        driver.findElement(By.xpath("//div[text() = '=']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class = 'qv3Wpe']")));
+
+    }
+
+
+}
---
+++ src/test/java/GoogleCalk/TestBase.java
@@ -0,0 +1,27 @@
+package GoogleCalk;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    String SITE_URL = "https://www.google.com/";
+    WebDriver driver;
+
+    @BeforeEach
+    public void start() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    public void finish() {
+        driver.quit();
+    }
+}
